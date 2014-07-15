a604ee77d9b28259d8273a54b482fe16a6932cb9
69edgy420me <jacksoncoll@live.com>
2014-07-15 22:40:52 +1000

    My greybox. Not sure if it helps.
    :+1:

---
+++ Assets/s_PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_PlayerControls : MonoBehaviour 
+{
+
+	public float playerSpeed;
+	public Vector2 jumpForce = new Vector3(0, 340);
+	public Vector2 jumpForceDown = new Vector3(0, -350);
+	
+	#region Start/Update
+	
+	void Start ()
+	{
+		//Original speed of player
+		playerSpeed = 4;
+	}
+	
+	void Update () 
+	{
+		//Left to right
+		float amtToMove = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+		transform.Translate (Vector3.right * amtToMove);
+		
+		if(Input.GetKeyDown ("space"))
+		{
+			rigidbody.velocity = Vector2.zero;
+			rigidbody.AddForce(jumpForce);
+		}
+		
+		if(Input.GetKeyUp ("space"))
+		{
+		rigidbody.velocity = Vector2.zero;
+		rigidbody.AddForce(jumpForceDown);
+		}
+	}
+
+	#endregion
+}
